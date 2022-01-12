aa8a1a276c8281b6a48f599dcfce31d3659b479f
formegusto <formegusto@gmail.com>
2022-01-12 17:05:14 +0900

    before eject

--- pre-research/script-file-input/src/App.tsx
+++ pre-research/script-file-input/src/App.tsx
@@ -1,37 +1,32 @@
 import React from "react";
-// import XLSX from "xlsx";
+import XLSXEx from "xlsx-extract";
 
 function App() {
-  // const onExcel = React.useCallback(
-  //   (e: React.ChangeEvent<HTMLInputElement>) => {
-  //     const reader = new FileReader();
+  const [filePath, setFilePath] = React.useState<string | null>(null);
+  const onExcel = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files)
+        if (e.target.files[0]) {
+          console.log("excel load start.");
+          console.log("startTime:", new Date().getTime());
+          const path = (e.target.files[0] as any).path;
 
-  //     reader.onload = (e) => {
-  //       const data = e.target?.result;
-  //       console.log(data && data.toString().length);
-  //       const wb = XLSX.read(data, { type: "binary" });
-  //       const ws = wb.Sheets[wb.SheetNames[0]];
-  //       console.log("WorkBook", wb);
-  //       console.log("WorkSheet", ws);
+          setFilePath(path);
+        }
+    },
+    []
+  );
 
-  //       // console.log(ws[XLSX.utils.encode_cell({ r: 6, c: 8 })]);
-  //       console.log("excel load end.");
-  //       console.log("endTime:", new Date().getTime());
-  //     };
-
-  //     if (e.target.files)
-  //       if (e.target.files[0]) {
-  //         console.log("excel load start.");
-  //         console.log("startTime:", new Date().getTime());
-  //         reader.readAsBinaryString(e.target.files[0]);
-  //       }
-  //   },
-  //   []
-  // );
+  React.useEffect(() => {
+    if (filePath) {
+      const XLSX = XLSXEx.XLSX;
+      console.log(XLSX);
+    }
+  }, [filePath]);
 
   return (
     <form>
-      <input type="file" name="file" />;
+      <input type="file" name="file" onChange={onExcel} />;
     </form>
   );
 }
