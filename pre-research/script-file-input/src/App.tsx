fd549378381c2e9304479c13a39b1c9663c5cc43
formegusto <formegusto@gmail.com>
2022-01-12 16:19:45 +0900

    xlsx test okay.

--- pre-research/script-file-input/src/App.tsx
+++ pre-research/script-file-input/src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import XLSX from "xlsx";
 
 function App() {
+  // const onExcel = React.useCallback(
+  //   (e: React.ChangeEvent<HTMLInputElement>) => {
+  //     const reader = new FileReader();
+
+  //     reader.onload = (e) => {
+  //       const data = e.target?.result;
+  //       console.log(data && data.toString().length);
+  //       const wb = XLSX.read(data, { type: "binary" });
+  //       const ws = wb.Sheets[wb.SheetNames[0]];
+  //       console.log("WorkBook", wb);
+  //       console.log("WorkSheet", ws);
+
+  //       // console.log(ws[XLSX.utils.encode_cell({ r: 6, c: 8 })]);
+  //       console.log("excel load end.");
+  //       console.log("endTime:", new Date().getTime());
+  //     };
+
+  //     if (e.target.files)
+  //       if (e.target.files[0]) {
+  //         console.log("excel load start.");
+  //         console.log("startTime:", new Date().getTime());
+  //         reader.readAsBinaryString(e.target.files[0]);
+  //       }
+  //   },
+  //   []
+  // );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <input type="file" name="file" />;
+    </form>
   );
 }
 
