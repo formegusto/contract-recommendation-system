0260e479fc3ce2638db3c0976544ebc4ea51a4af
formegusto <formegusto@gmail.com>
2022-01-13 12:21:27 +0900

    js-python test okay

--- pre-research/js-python/python/index.py
+++ pre-research/js-python/python/index.py
@@ -1,6 +1,19 @@
+import sys
+
+
 def getValue():
     print("value")
 
 
+def getValueArgs(name, age):
+    print("value")
+    print(name, age)
+
+
 if __name__ == "__main__":
-    getValue()
+    if len(sys.argv) == 3:
+        name = sys.argv[1]
+        age = sys.argv[2]
+        getValueArgs(name, age)
+    else:
+        getValue()
--- pre-research/js-python/src/index.ts
+++ pre-research/js-python/src/index.ts
@@ -1,8 +1,16 @@
 import childProcess from "child_process";
 
 const spawn = childProcess.spawn;
+
 const result = spawn("python3", ["python/index.py"]);
+const result_argv = spawn("python3", ["python/index.py", "formegusto", "27"]);
 
 result.stdout.on("data", (data: any) => {
+  console.log("파이썬 실행완료");
+  console.log(data.toString());
+});
+
+result_argv.stdout.on("data", (data: any) => {
+  console.log("파이썬(argv) 실행완료");
   console.log(data.toString());
 });
